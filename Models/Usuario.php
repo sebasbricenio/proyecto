87fb414a4b833fedca7d4fa208a24d889f7c0464
sebasbricenio <38321595+sebasbricenio@users.noreply.github.com>
2018-06-29 20:30:01 -0300

    Add files via upload

--- Models/Usuario.php
+++ Models/Usuario.php
@@ -1,1 +1,34 @@
+<?php
 
+namespace Proyecto\Models;
+
+
+class Usuario {
+
+  protected $nombre;
+  protected $email;
+  protected $pass;
+
+
+  public function __construct($nombre, $email, $pass) {
+    $this->nombre = $nombre;
+    $this->email = $email;
+    $this->pass = $pass;
+
+  }
+
+  public function hashearPassword() {
+
+    $this->pass = password_hash($this->pass, PASSWORD_DEFAULT);
+      $crearUsuario = [
+        "nombre" => $this->nombre,
+        "pass" => $this->pass,
+        "email" => $this->email,
+      ];
+    return $crearUsuario;
+  }
+}
+
+
+
+ ?>
